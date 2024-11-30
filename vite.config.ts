939707d1c9409a120876ea80a51f292ed1e101ea
yongzhe525 <renyz0525@naver.com>
2024-11-30 14:15:53 +0900

    update deployment modification

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base : "absproxy/5173"
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/absproxy/5173/' : './'
+
+  return {
+    plugins: [react()],
+    base
+  };
 })
